a3e61f8d5a1935cf35f7926e6a1f769fe5614d12
Haohao123coding <18217288892@163.com>
2025-08-02 00:06:08 +0800

    Update 4 files

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,1 +1,29 @@
-// Oh, no any scripts
+const topBarItems = {
+    1: {
+        urlOfA: "https://haohao123coding.github.io/main/",
+        text: "主页"
+    },
+    2: {
+        urlOfA: "https://haohao123coding.github.io/main/blogmain",
+        text: "博客"
+    },
+    3: {
+        urlOfA: "https://haohao123coding.github.io/main/ac_records",
+        text: "AC 记录"
+    }
+}
+
+function showBar(){
+    const topBarContainer = document.getElementById("top-bar-items-id");
+    topBarContainer.innerHTML = `
+    ${Object.entries(topBarItems).map(([num, item]) => {
+        return `
+            <a href="${item.urlOfA}"><button class="btntp">${item.text}</button></a>
+        `;
+    }).join("")}
+    `
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    showBar();
+});
