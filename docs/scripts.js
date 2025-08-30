741c0293a62cabd720c90a950db04ab231131630
Haohao123coding <18217288892@163.com>
2025-08-30 11:24:26 +0800

    重构完成 1

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,18 +1,3 @@
-const topBarItems = [
-    {
-        urlOfA: "https://haohao123coding.github.io/main/",
-        text: "主页"
-    },
-    {
-        urlOfA: "https://haohao123coding.github.io/main/blogmain",
-        text: "博客"
-    },
-    {
-        urlOfA: "https://haohao123coding.github.io/main/ac_records",
-        text: "AC 记录"
-    }
-]
-
 const myAccounts = [
     {
         urlOfA: "https://github.com/Haohao123coding",
@@ -36,40 +21,15 @@ const myAccounts = [
     }
 ]
 
-function showBar(){
-    const topBarContainer = document.getElementById("top-bar-items-id");
-    topBarContainer.innerHTML = `
-    ${topBarItems.map(item => {
-        return `<a href="${item.urlOfA}"><button class="btntp">${item.text}</button></a>`;
-    }).join("\n")}
-    `
-}
-
 function showMyAccounts(){
     const accountContainer = document.getElementById("accounts-id");
     accountContainer.innerHTML = `
     ${myAccounts.map(item => {
-        return `<a href="${item.urlOfA}" class = "outlink">${item.text}</a>`;
+        return `<button onclick = "window.open('${item.urlOfA}')" class = "btntp">${item.text}</button>`;
     }).join("\n")}
     `
 }
 
-function showAnnouncement(){
-    const announcementContainer = document.getElementById("announcement-id");
-    announcementContainer.innerHTML = `
-    <button class = "btnclose" onclick = "closeAnnouncement()">关闭</button>
-    <h3>公告 / Announcement</h3>
-    <p>重构即将完成，敬请谅解。</p>
-    <p>Refactoring is about to be completed, please understand.</p>
-    `
-}
-
-function closeAnnouncement(){
-    document.querySelector(".announcement").style.display = "none";
-}
-
 document.addEventListener('DOMContentLoaded', function(){
-    showBar();
     showMyAccounts();
-    showAnnouncement();
 });
